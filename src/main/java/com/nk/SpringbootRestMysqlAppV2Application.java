b40c7827ad2cff87be7d0d904da6db8bcc926cdf
Neeraj <neerajk.azad@gmail.com>
2025-07-17 22:40:51 +0530

    Initial commit

---
+++ src/main/java/com/nk/SpringbootRestMysqlAppV2Application.java
@@ -0,0 +1,13 @@
+package com.nk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestMysqlAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestMysqlAppV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nk/SpringbootRestMysqlAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestMysqlAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
